e5cf9b033471043ca20298698768ed158b102044
daoki7 <115014406+daoki7@users.noreply.github.com>
2023-05-08 13:26:40 +0900

    Server.java 待ちプレイヤ確認応答スレッドなど

--- src/Server.java
+++ src/Server.java
@@ -9,13 +9,23 @@ public class Server{
 	private int port; // サーバの待ち受け用ポート
 	private PrintWriter out; //データ送信用オブジェクト
 	private Receiver receiver; //データ受信用オブジェクト
+	
+	static boolean[] RoomInfo = {false, false, false};
+	
 
 	//コンストラクタ
 	public Server(int port) { //待ち受けポートを引数とする
 		this.port = port; //待ち受けポートを渡す
 	}
-
-	// データ受信用スレッド(内部クラス)
+	
+	//待ちプレイヤ情報出力
+	public static boolean[] GetRoomInfo() {
+		return RoomInfo;
+	}
+	
+	//待ちプレイヤ更新メソッド
+	
+	//Sample// データ受信用スレッド(内部クラス)
 	class Receiver extends Thread {
 		private InputStreamReader sisr; //受信データ用文字ストリーム
 		private BufferedReader br; //文字ストリーム用のバッファ
@@ -29,6 +39,7 @@ public class Server{
 				System.err.println("データ受信時にエラーが発生しました: " + e);
 			}
 		}
+		
 		// 内部クラス Receiverのメソッド
 		public void run(){
 			try{
@@ -42,6 +53,65 @@ public class Server{
 				}
 			} catch (IOException e){ // 接続が切れたとき
 				System.err.println("クライアントとの接続が切れました．");
+			}
+		}
+	}
+	
+
+	//マッチングスレッド
+	class MatchThread extends Thread{
+		
+	}
+	
+	//対局スレッド
+	class GameThread{
+		int RoomID;
+		String P1_name;//先攻
+		String P2_name;//後攻
+		int time;//開始時の残り時間
+		Socket P1_socket;//先攻のソケット
+		Socket P2_socket;//後攻のソケット
+		
+		
+		//コンストラクタ
+		GameThread(int id){
+			P1_name = null;
+			P2_name = null;
+			time = 0;
+			RoomID = id
+		}
+		
+		//待機プレイヤの有無を返す
+		public boolean isWaiting() {
+			if(P2_name != null) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		
+		//プレイヤを部屋に入れる
+		public void setPlayer(Socket sc, String name, boolean isFirst) {
+			if(isFirst) {
+				P1_name = name;
+				P1_socket = sc;
+			}
+			else {
+				P2_name = name;
+				P2_socket = sc;
+			}
+		}
+		
+		//ゲーム開始時の残り時間を設定する
+		public void setTime(int t) {
+			time = t;
+		}
+		
+		//runメソッド
+		public void run() {
+			while(P2_name == null) {
+				
 			}
 		}
 	}
@@ -63,9 +133,38 @@ public class Server{
 			System.err.println("ソケット作成時にエラーが発生しました: " + e);
 		}
 	}
-
-	public static void main(String[] args){ //main
+	
+	//mainメソッド
+	public static void main(String[] args){
 		Server server = new Server(10000); //待ち受けポート10000番でサーバオブジェクトを準備
 		server.acceptClient(); //クライアント受け入れを開始
+		RoomInfoThread rit = new RoomInfoThread(10000);
+		
+	}
+}
+
+//待ちプレイヤ確認応答スレッド
+class RoomInfoThread extends Thread{
+	int Info_port;
+	private PrintWriter Info_out; //データ送信用オブジェクト
+	//コンストラクタ
+	RoomInfoThread(int port){
+		Info_port = port+1; //port+1番のポートを待ちプレイヤ確認応答スレッドに使用する
+	}
+	
+	//run
+	public void run() {
+		while(true) {
+			try {
+				ServerSocket ri_ss = new ServerSocket(Info_port);
+				Socket ri_socket = ri_ss.accept();
+				Info_out.println(Server.GetRoomInfo()); //待ち状況を書き込む
+				Info_out.flush(); //待ち状況を送信する
+				ri_ss.close();
+				ri_socket.close();
+			} catch (IOException e) {
+				System.err.println("クライアントとの接続が切れました．");
+			}
+		}
 	}
 }
