1f47971a42de4a07ff46ee5c771932fc95825e1a
Michi-851929 <ko.kutsumi@icloud.com>
2023-05-09 16:10:01 +0900

    Base Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,68 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Client {
+import javax.swing.JFrame;
 
-    public static void main(String[] args) {
-        // TD 自動生成されたメソッド・スタブ
+public class Client extends JFrame implements ActionListener{
+
+	Client()
+	{
+		
+	}
+	
+	public void changePhase()
+	{
+		
+	}
+	
+	public void reloadDisplay()
+	{
+		
+	}
+	
+	public String[] getWaitingPlayers()
+	{
+		
+	}
+	
+	public String[] accessServer()
+	{
+		
+	}
+	
+	public boolean reactConnection()
+	{
+		
+	}
+	
+	public void doMyTurn()
+	{
+		
+	}
+	
+	public void doYourTurn()
+	{
+		
+	}
+	
+	public int[] getCommand()
+	{
+		
+	}
+	
+	public void endBattle()
+	{
+		
+	}
+	
+	public void actionPerformed(ActionEvent ae)
+	{
+		
+	}
+	
+    public static void main(String[] args)
+    {
+        
 
     }
 
