ac2d5f28d706997acd7e6434fa071c62cc65a51d
Michi-851929 <ko.kutsumi@icloud.com>
2023-05-13 08:45:55 +0900

    UI Update

--- src/Client.java
+++ src/Client.java
@@ -1,31 +1,250 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import javax.swing.JFrame;
 
-public class Client extends JFrame implements ActionListener{
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class Client extends JFrame implements ActionListener, FocusListener{
 
 	private static final int SERVER_PORT_1 = 10000;//接続確認に使うほうはこちら
 	private static final int COMMAND_CHECK_INTERVAL = 500; // 0.5秒ごとにgetCommandメソッドを監視する
 	private static final int HEARTBEAT_INTERVAL = 1000; // 1秒ごとにサーバーにハートビートを送信する
 	private static final int TIMEOUT_INTERVAL = 5000; // 5秒サーバーからのレスポンスがなかったらタイムアウトする
-	Client()
+	private static final int PHASE_TITLE = 0; //changePhase()の引数でタイトル画面への遷移を表す
+	private static final int PHASE_BATTLE = 1; //changePhase()の引数で対局画面への遷移を表す
+	private static final int PHASE_RESULT = 2; //changePhase()の引数で結果画面への遷移を表す
+	
+	Othello othello;
+	
+	JPanel display = new JPanel();
+	
+	//タイトル画面のオブジェクト
+	JTextField ui_tf_namefield;
+	JButton ui_jb_5min;
+	JButton ui_jb_10min;
+	JButton ui_jb_20min;
+	JLabel ui_jl_5min;
+	JLabel ui_jl_10min;
+	JLabel ui_jl_20min;
+	JButton ui_jb_start;
+	
+	//対局画面のオブジェクト
+	JLabel ui_jl_name1;
+	JLabel ui_jl_name2;
+	JLabel ui_jl_time1;
+	JLabel ui_jl_time2;
+	JLabel ui_jl_nstones1;
+	JLabel ui_jl_nstones2;
+	JButton ui_jb_giveup;
+	JButton[][] ui_jb_field = new JButton[8][8];
+	
+	Client(String title)
 	{
+		super(title);
+		setLayout(new FlowLayout());
 		
+		//スタート画面
+		JPanel start_display = new JPanel();
+		start_display.setLayout(new BorderLayout());
+		JLabel ui_jl_logo1 = new JLabel(new ImageIcon("img/othellogo.png"));
+		start_display.add(ui_jl_logo1, "Center");
+		start_display.add(new JLabel("通信中・・・", SwingConstants.CENTER), "South");
+		display.add(start_display);
+		add(display);
+		
+		//接続を待つ
+		
+		//タイトル画面
+		//changePhase(PHASE_TITLE);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setVisible(true);
 	}
 
-	public void changePhase()
+	public void changePhase(int phase)
 	{
+		switch(phase) {
+		//タイトル画面
+		case PHASE_TITLE:
+			display.removeAll();
+			JPanel title = new JPanel();
+			title.setLayout(new BorderLayout(0, 30));
+			
+			//ロゴとプレイヤ名入力エリア
+			JPanel p00 = new JPanel();
+			p00.setLayout(new BorderLayout());
+			JLabel ui_jl_logo2 = new JLabel(new ImageIcon("img/othellogo2.png"));
+			ui_tf_namefield = new JTextField("プレイヤ名");
+			ui_tf_namefield.setForeground(Color.LIGHT_GRAY);
+			ui_tf_namefield.addFocusListener(this);
+			ui_tf_namefield.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			p00.add(ui_jl_logo2, "North");
+			p00.add(ui_tf_namefield, "Center");
+			title.add(p00, "North");
+			
+			//持ち時間選択ボタン
+			JPanel p01 = new JPanel();
+			p01.setLayout(new BorderLayout(0, 10));
+			JPanel p02 = new JPanel();
+			p02.setLayout(new BorderLayout());
+			JPanel p03 = new JPanel();
+			p03.setLayout(new GridLayout(1, 3, 10, 0));
+			JPanel p04 = new JPanel();
+			p04.setLayout(new GridLayout(1, 3));
+			JLabel ui_jl_waittime = new JLabel("持ち時間", SwingConstants.CENTER);
+			ui_jl_waittime.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 16));
+			ui_jb_5min = new JButton("5分");
+			ui_jb_5min.addActionListener(this);
+			ui_jb_5min.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jb_10min = new JButton("10分");
+			ui_jb_10min.addActionListener(this);
+			ui_jb_10min.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jb_20min = new JButton("20分");
+			ui_jb_20min.addActionListener(this);
+			ui_jb_20min.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jl_5min = new JLabel("×", SwingConstants.CENTER);
+			ui_jl_5min.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jl_10min = new JLabel("×", SwingConstants.CENTER);
+			ui_jl_10min.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jl_20min = new JLabel("×", SwingConstants.CENTER);
+			ui_jl_20min.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			JLabel ui_jl_waiting = new JLabel("待機プレイヤ", SwingConstants.CENTER);
+			ui_jl_waiting.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 16));
+			p03.add(ui_jb_5min, "A");
+			p03.add(ui_jb_10min, "B");
+			p03.add(ui_jb_20min, "C");
+			p04.add(ui_jl_5min, "A");
+			p04.add(ui_jl_10min, "B");
+			p04.add(ui_jl_20min, "C");
+			p02.add(p03, "Center");
+			p02.add(p04, "South");
+			p01.add(ui_jl_waittime, "North");
+			p01.add(p02, "Center");
+			p01.add(ui_jl_waiting, "South");
+			title.add(p01, "Center");
+			
+			//開始/中止ボタン
+			JPanel p05 = new JPanel();
+			p05.setLayout(new FlowLayout());
+			ui_jb_start = new JButton("開始");
+			ui_jb_start.addActionListener(this);
+			ui_jb_start.setPreferredSize(new Dimension(200, 50));
+			ui_jb_start.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			p05.add(ui_jb_start);
+			title.add(p05, "South");
+			
+			display.add(title);
+			setVisible(true);
+			repaint();
+			break;
+			
+		//対局画面
+		case PHASE_BATTLE:
+			display.removeAll();
+			JPanel battle = new JPanel();
+			battle.setLayout(new BorderLayout(10, 10));
+			
+			JPanel p06 = new JPanel();
+			p06.setLayout(new FlowLayout(FlowLayout.RIGHT, 20, 0));
+			ui_jl_name1 = new JLabel(othello.getPlayers()[0].getPlayerName());
+			ui_jl_name1.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jl_time1 = new JLabel("5:00");
+			ui_jl_time1.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			p06.add(ui_jl_name1);
+			p06.add(ui_jl_time1);
+			battle.add(p06, "South");
+			
+			JPanel p07 = new JPanel();
+			p07.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 0));
+			ui_jl_name2 = new JLabel(othello.getPlayers()[1].getPlayerName());
+			ui_jl_name2.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			ui_jl_time2 = new JLabel("5:00");
+			ui_jl_time2.setFont(new Font("ＭＳ ゴシック", Font.PLAIN, 24));
+			p07.add(ui_jl_time2);
+			p07.add(ui_jl_name2);
+			battle.add(p07, "North");
+			
+			JPanel p08 = new JPanel();
+			p08.setLayout(new GridLayout(2, 1));
+			ui_jl_nstones1 = new JLabel("3");
+			ui_jl_nstones2 = new JLabel("3");
+			p08.add(ui_jl_nstones1, "A");
+			p08.add(ui_jl_nstones2, "B");
+			battle.add(p08, "West");
+			
+			JPanel p09 = new JPanel();
+			p09.setLayout(new BorderLayout());
+			ui_jb_giveup = new JButton("投了");
+			p09.add(ui_jb_giveup, "South");
+			battle.add(p09, "East");
+			
+			JPanel p10 = new JPanel();
+			p10.setLayout(new GridLayout(8, 8, -13, -8));
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					ui_jb_field[i][j] = new JButton();//Integer.toString(i) + Integer.toString(j));
+					ui_jb_field[i][j].setIcon(getStoneIcon());
+					p10.add(ui_jb_field[i][j]);
+				}
+			}
+			battle.add(p10);
+			
+			display.add(battle);
+			setVisible(true);
+			repaint();
+			break;
+		case PHASE_RESULT:
+			
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private ImageIcon getStoneIcon()
+	{
+		Image img = createImage(50, 50);
+		Graphics g = img.getGraphics();
+		g.setColor(Color.GREEN);
+		for(int i = 0; i < 50; i++) {
+			g.drawLine(i, 0, i, 50 - 1);
+		}
+		g.setColor(Color.GRAY);
+		g.drawRect(0, 0, 50 - 1, 50 - 1);
 		
+		ImageIcon icon = new ImageIcon(img);
+		return icon;
 	}
 
 	public void reloadDisplay()
 	{
 		
 	}
-	
+	/*
 	public String[] getWaitingPlayers()
 	{
 		
@@ -40,7 +259,7 @@ public class Client extends JFrame implements ActionListener{
 	{
 		
 	}
-
+*/
 
 
 
@@ -199,11 +418,31 @@ public class Client extends JFrame implements ActionListener{
 		
 	}
 	
+	public void focusGained(FocusEvent fe)
+	{
+		String s = ui_tf_namefield.getText();
+		if(s.equals("プレイヤ名")) {
+			ui_tf_namefield.setText("");
+			ui_tf_namefield.setForeground(Color.BLACK);
+		}
+	}
+	
+	public void focusLost(FocusEvent fe)
+	{
+		String s = ui_tf_namefield.getText();
+		if(s.equals("")) {
+			ui_tf_namefield.setText("プレイヤ名");
+			ui_tf_namefield.setForeground(Color.LIGHT_GRAY);
+		}
+	}
+	
     public static void main(String[] args)
     {
-        
+        Client client = new Client("Othello Game");
+        client.changePhase(PHASE_TITLE);
+        client.othello = new Othello(new Player("aiueo", true, 800), new Player("oeuia", false, 800));
+        client.changePhase(PHASE_BATTLE);
 
     }
-
 
 }
