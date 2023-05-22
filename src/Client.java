7e612b1cfc8106a649cd1836cc742e7950acdeb7
Michi-851929 <ko.kutsumi@icloud.com>
2023-05-23 05:02:27 +0900

    Client Update

--- src/Client.java
+++ src/Client.java
@@ -377,38 +377,57 @@ public class Client extends JFrame implements ActionListener, FocusListener {
 
 	public void reloadDisplay(int[] play) {
 		System.out.println("applyMove: (" + play[0] + ", " + play[1] + ")");
-		boolean[][] change_board = othello.applyMove(play);
-		int[][] board = othello.getBoard();
-		int count_black = 0;
-		int count_white = 0;
-		(ui_jb_field[play[0]][play[1]])
-				.setDisabledIcon(getStoneIcon((othello.getCurrentTurn() ? Othello.WHITE : Othello.BLACK), 0));
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				int di = i;
-				int dj = j;
-				if (change_board[i][j]) {
-					new Thread(() -> {
-						try {
-							for (int k = 0; k <= 360; k += 2) {
-								Thread.sleep(2);
-								ui_jb_field[di][dj].setDisabledIcon(
-										getStoneIcon(othello.getCurrentTurn() ? Othello.BLACK : Othello.WHITE, k));
+		switch(othello.checkWinner()) {
+		case 3:
+			eob_flag = true;
+			break;
+		case -3:
+			eob_flag = true;
+			break;
+		case 1:
+			eob_flag = true;
+			break;
+		case -1:
+			eob_flag = true;
+			break;
+		case 0:
+			eob_flag = true;
+			break;
+		default:
+			boolean[][] change_board = othello.applyMove(play);
+			int[][] board = othello.getBoard();
+			int count_black = 0;
+			int count_white = 0;
+			(ui_jb_field[play[0]][play[1]])
+					.setDisabledIcon(getStoneIcon((othello.getCurrentTurn() ? Othello.WHITE : Othello.BLACK), 0));
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					int di = i;
+					int dj = j;
+					if (change_board[i][j]) {
+						new Thread(() -> {
+							try {
+								for (int k = 0; k <= 360; k += 2) {
+									Thread.sleep(2);
+									ui_jb_field[di][dj].setDisabledIcon(
+											getStoneIcon(othello.getCurrentTurn() ? Othello.BLACK : Othello.WHITE, k));
+								}
+							} catch (Exception e) {
+								e.printStackTrace();
 							}
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}).start();
-				}
-				if (board[i][j] == Othello.BLACK) {
-					count_black++;
-				} else if (board[i][j] == Othello.WHITE) {
-					count_white++;
+						}).start();
+					}
+					if (board[i][j] == Othello.BLACK) {
+						count_black++;
+					} else if (board[i][j] == Othello.WHITE) {
+						count_white++;
+					}
 				}
 			}
+			ui_jl_nstones1.setText("× " + (count_black >= 10 ? "" : " ") + count_black + " ");
+			ui_jl_nstones2.setText("× " + (count_white >= 10 ? "" : " ") + count_white + " ");
+			break;
 		}
-		ui_jl_nstones1.setText("× " + (count_black >= 10 ? "" : " ") + count_black + " ");
-		ui_jl_nstones2.setText("× " + (count_white >= 10 ? "" : " ") + count_white + " ");
 	}
 
 	public void doMyTurn() {
